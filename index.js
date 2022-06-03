299bc46d12382f05d98113585daea4eeb4fbe1d6
Kevin Onyancha <64799668+Konyancha1@users.noreply.github.com>
2022-06-03 14:03:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+function generateRandomInteger(min, max) {
+  return Math.floor(min + Math.random()*(max - min + 1));
+}
+
+const min = 1;
+var max = 2;
+var points = 0;
+const real = generateRandomInteger(min, max);
+const name = prompt("Enter username:")
+
+const guess_game = () => {
+  const guess = prompt(`Guess between 1 and ${max}`);
+  const reserve = new Promise(function (resolve, reject) {
+    if(guess == real){
+      const score = {
+        points: points+=1
+      };
+      resolve(score);
+    }
+    else {
+      reject(new Error(guess_game()));
+    }
+  })
+
+  const game = async() => {
+    try {
+          const clear = await reserve;
+          const ask = prompt("Continue to play? y or n");
+          if (ask == "y") {
+              max++;
+              generateRandomInteger(min, max);
+              guess_game();
+          }
+          else {
+              console.log(`Thank you for playing ${name}. Your score is ${clear.points}`);
+          }
+      } catch (err) {
+          console.log(err.message);
+      }
+  }
+    game()
+}
+
+guess_game();
